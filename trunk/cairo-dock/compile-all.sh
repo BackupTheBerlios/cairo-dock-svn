fc8a12dab981afc2be422fd0ec21440fb300d43c
fabounet <fabounet@620951bb-bb42-0410-82c3-830e739ed170>
2008-03-10 03:02:59 +0000

    normalisation des themes et deplacement a la racine

--- trunk/cairo-dock/compile-all.sh
+++ trunk/cairo-dock/compile-all.sh
@@ -6,27 +6,41 @@ export CAIRO_DOCK_AUTORECONF="0"
 export CAIRO_DOCK_CLEAN="0"
 export CAIRO_DOCK_COMPIL="0"
 export CAIRO_DOCK_INSTALL="0"
+export CAIRO_DOCK_THEMES="0"
 export CAIRO_DOCK_EXCLUDE="template"
 export CAIRO_DOCK_EXTRACT_MESSAGE=${CAIRO_DOCK_DIR}/utils/extract-message
 export CAIRO_DOCK_GEN_TRANSLATION=${CAIRO_DOCK_DIR}/cairo-dock/po/generate-translation.sh
 
-if test "$1" = "-a" -o "$2" = "-a" -o "$3" = "-a" -o "$4" = "-a"; then
-	export CAIRO_DOCK_AUTORECONF="1"
-fi
-if test "$1" = "-i" -o "$2" = "-i" -o "$3" = "-i" -o "$4" = "-i"; then
-	export CAIRO_DOCK_INSTALL="1"
-fi
-if test "$1" = "-c" -o "$2" = "-c" -o "$3" = "-c" -o "$4" = "-c"; then
-	export CAIRO_DOCK_CLEAN="1"
-fi
-if test "$1" = "-C" -o "$2" = "-C" -o "$3" = "-C" -o "$4" = "-C"; then
-	export CAIRO_DOCK_COMPIL="1"
-fi
+while getopts "acCit" flag
+do
+	echo "$flag" $OPTIND $OPTARG
+	case "$flag" in
+	a)
+		export CAIRO_DOCK_AUTORECONF="1"
+		;;
+	c)
+		export CAIRO_DOCK_CLEAN="1"
+		;;
+	C)
+		export CAIRO_DOCK_COMPIL="1"
+		;;
+	i)
+		export CAIRO_DOCK_INSTALL="1"
+		;;
+	t)
+		export CAIRO_DOCK_THEMES="1"
+		;;
+	*)
+		echo "unexpected argument"
+		;;
+	esac
+done
 
 
 cd $CAIRO_DOCK_DIR
 find . -name linguas -execdir mv linguas LINGUAS \;
 find . -name potfiles.in -execdir mv potfiles.in POTFILES.in \;
+
 
 cd cairo-dock
 if test "$CAIRO_DOCK_CLEAN" = "1"; then
@@ -59,10 +73,31 @@ if test "$CAIRO_DOCK_INSTALL" = "1"; then
 	sudo chmod +x $CAIRO_DOCK_PREFIX/bin/cairo-dock-update.sh
 	sudo chmod +x $CAIRO_DOCK_PREFIX/bin/launch-cairo-dock-after-beryl.sh
 fi
-cd ../plug-ins
 
 
+cd ..
+if test "$CAIRO_DOCK_THEMES" = "1"; then
+	cd themes
+	if test "$CAIRO_DOCK_CLEAN" = "1"; then
+		rm -f config.* configure configure.lineno intltool-extract intltool-merge intltool-update libtool ltmain.sh Makefile.in Makefile aclocal.m4 install-sh install depcomp missing compile stamp-h1 autom4te.cache
+		find . -name Makefile -delete
+		find . -name Makefile.in -delete
+	fi
+	if test "$CAIRO_DOCK_AUTORECONF" = "1"; then
+		autoreconf -isvf && ./configure
+	fi
+	if test "$CAIRO_DOCK_COMPIL" = "1"; then
+		make
+	fi
+	if test "$CAIRO_DOCK_INSTALL" = "1"; then
+		echo "installation des themes ..."
+		sudo make install
+	fi
+	cd ..
+fi
 
+
+cd plug-ins
 for plugin in *
 do
 	if test -d $plugin; then
